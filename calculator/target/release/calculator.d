47cadf7decc3eaad89e9c43a3dc72d0f813a1bb2
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-04 12:22:32 +0200

    CLI Calculator done 🎉

---
+++ calculator/target/release/calculator.d
@@ -0,0 +1,1 @@
+/Users/bensparks/Documents/GitHub/learning-rust/calculator/target/release/calculator: /Users/bensparks/Documents/GitHub/learning-rust/calculator/src/main.rs
---
+++ calculator/target/release/deps/calculator-b3ca8c7afb991da5.d
@@ -0,0 +1,5 @@
+/Users/bensparks/Documents/GitHub/learning-rust/calculator/target/release/deps/calculator-b3ca8c7afb991da5: src/main.rs
+
+/Users/bensparks/Documents/GitHub/learning-rust/calculator/target/release/deps/calculator-b3ca8c7afb991da5.d: src/main.rs
+
+src/main.rs:
