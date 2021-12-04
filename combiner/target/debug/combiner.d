726935a58daddba649027a9624a72fb8cffe0d9a
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-04 12:33:17 +0200

    Implementing Args struct

---
+++ combiner/target/debug/combiner.d
@@ -0,0 +1,1 @@
+/Users/bensparks/Documents/GitHub/learning-rust/combiner/target/debug/combiner: /Users/bensparks/Documents/GitHub/learning-rust/combiner/src/args.rs /Users/bensparks/Documents/GitHub/learning-rust/combiner/src/main.rs
---
+++ combiner/target/debug/deps/combiner-951f620b183cb333.d
@@ -0,0 +1,6 @@
+/Users/bensparks/Documents/GitHub/learning-rust/combiner/target/debug/deps/combiner-951f620b183cb333: src/main.rs src/args.rs
+
+/Users/bensparks/Documents/GitHub/learning-rust/combiner/target/debug/deps/combiner-951f620b183cb333.d: src/main.rs src/args.rs
+
+src/main.rs:
+src/args.rs:
