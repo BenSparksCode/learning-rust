8f7e337257963a916b487739effb5a236234f62d
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-04 11:56:03 +0200

    Basic cli calc working

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -4,11 +4,27 @@ fn main() {
     let mut args: Args = args();
 
     let first  = args.nth(1).unwrap();
-    let operator = args.nth(0).unwrap();
+    let operator = args.nth(0).unwrap().chars().next().unwrap();
     let second = args.nth(0).unwrap();
 
     let first_num = first.parse::<f32>().unwrap();
     let second_num = second.parse::<f32>().unwrap();
 
-    println!("{:?} {} {}", first, operator, second);
+    let result = operate(operator, first_num, second_num);
+
+    println!("{:?} {} {} = {}", first, operator, second, result);
+}
+
+fn operate(operator: char, first_num: f32, second_num: f32) -> f32 {
+    if operator == '+' {
+        return first_num + second_num
+    } else if operator == '-' {
+        return first_num - second_num
+    } else if operator == '*' {
+        return first_num * second_num
+    } else if operator == '/' {
+        return first_num / second_num
+    } else {
+        0.0
+    }
 }
