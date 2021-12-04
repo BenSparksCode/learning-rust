a2e9a90b33bddb447473e9871335430c32223520
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-04 11:51:07 +0200

    Parsing nums for calc operation

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -1,3 +1,14 @@
+use std::env::{args, Args};
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Args = args();
+
+    let first  = args.nth(1).unwrap();
+    let operator = args.nth(0).unwrap();
+    let second = args.nth(0).unwrap();
+
+    let first_num = first.parse::<f32>().unwrap();
+    let second_num = second.parse::<f32>().unwrap();
+
+    println!("{:?} {} {}", first, operator, second);
 }
