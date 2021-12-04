99a50db74734121b71583a5e7642741d65666469
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-04 11:34:28 +0200

    Hello world in Rust lfg 🎉

---
+++ HelloWorld/target/debug/HelloWorld.d
@@ -0,0 +1,1 @@
+/Users/bensparks/Documents/GitHub/learning-rust/HelloWorld/target/debug/HelloWorld: /Users/bensparks/Documents/GitHub/learning-rust/HelloWorld/src/main.rs
---
+++ HelloWorld/target/debug/deps/HelloWorld-7f601551cb926ff1.d
@@ -0,0 +1,5 @@
+/Users/bensparks/Documents/GitHub/learning-rust/HelloWorld/target/debug/deps/HelloWorld-7f601551cb926ff1: src/main.rs
+
+/Users/bensparks/Documents/GitHub/learning-rust/HelloWorld/target/debug/deps/HelloWorld-7f601551cb926ff1.d: src/main.rs
+
+src/main.rs:
