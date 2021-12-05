29e4ea6aab55285c62b7c758dc6d4655502309c5
Ben Sparks <52714090+BenSparksCode@users.noreply.github.com>
2021-12-05 07:07:54 +0200

    Added some error handling to combiner

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -6,10 +6,19 @@ use std::{
     io::BufReader
 };
 
-fn main() {
+enum ImageDataErrors {
+    DifferentImageFormats,
+}
+
+fn main() -> Result<(), ImageDataErrors>{
     let args = Args::new();
     let (image_1, image_format_1) = find_image_from_path(args.image_1);
     let (image_2, image_format_2) = find_image_from_path(args.image_2);
+
+    if(image_format_1 != image_format_2) {
+        return Err(ImageDataErrors::DifferentImageFormats);
+    }
+    Ok(())
 }
 
 fn find_image_from_path(path: String) -> (DynamicImage, ImageFormat) {
